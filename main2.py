8e1fcfa4960fa811b80d29e81c62b446b4af7363
Nelson Malbone <nhmalbone@icloud.com>
2024-08-21 17:56:31 -0400

    08/21/2024 C to K is completed in error free

--- main2.py
+++ main2.py
@@ -2,11 +2,12 @@
 # this time we will print out the following
 # using kelvin conversion
 
-user_Unit_Input = input("Is this conversion for ('K') Kelvin, ('C') Celsius, ('F') Fahrenheit: ")
-user_Temp_Input = input("Enter Temperature to Convert: ")
+user_Unit_Input = input("Is this conversion for ('K') Kelvin, ('C') Celsius: ")
+user_Temp_Input = float(input("Enter Temperature to Convert: "))
 
 if user_Unit_Input == 'K':
-    pass
+    user_Temp_Input = round(user_Temp_Input - 273.15, 2)
+    print(f'The temperature in Celsius is: {user_Temp_Input}°C')
 elif user_Unit_Input == 'C':
     pass
 else:
