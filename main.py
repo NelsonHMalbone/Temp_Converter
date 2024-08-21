177a90ad4e6914b0cf4685927c6e9bdaa5ca20d5
Nelson Malbone <nhmalbone@icloud.com>
2024-08-21 17:24:46 -0400

    08/21/2024 have a error message and adjust the first input

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # youtube link: https://youtu.be/4wGuB3oAKc4?si=seYSW2YglJxpENl8&t=619
 
 # asking current degree of temp
-unit = input("Is this temperature in Celsius or Fahrenheit? (C/F)")
+unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
 # asking for temp
 # what this in a float because of numbers in deciminals
 temp = float(input("Enter the Temperature: "))
@@ -14,4 +14,4 @@ if unit == 'C':
 elif unit == 'F':
     pass
 else:
-    pass
+    print(f"{unit} is not a valid unit of measurement please pick C for celsius or F for fahrenheit")
