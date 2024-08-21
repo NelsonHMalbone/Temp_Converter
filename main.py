fc0e0558e010830a2d0b6971b21c528ddf853f8c
Nelson Malbone <nhmalbone@icloud.com>
2024-08-21 17:37:12 -0400

    08/21/2024 have the conversion for f to c completed

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ if unit == 'C':
     temp = round((9 * temp) / 5 + 32, 1)
     print(f'The temperature in Fahrenheit is: {temp}°F')
 elif unit == 'F':
-    pass
+    temp = round((temp - 32) * 5 / 9, 1)
+    print(f'The temperature in Celsius is: {temp}°C')
 else:
     print(f"{unit} is not a valid unit of measurement please pick C for celsius or F for fahrenheit")
