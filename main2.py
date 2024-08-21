985651e6ad31c6f3b7c6842750bd515552512992
Nelson Malbone <nhmalbone@icloud.com>
2024-08-21 17:59:17 -0400

    08/21/2024 K to c is completed in error free

--- main2.py
+++ main2.py
@@ -9,6 +9,7 @@ if user_Unit_Input == 'K':
     user_Temp_Input = round(user_Temp_Input - 273.15, 2)
     print(f'The temperature in Celsius is: {user_Temp_Input}°C')
 elif user_Unit_Input == 'C':
-    pass
+    user_Temp_Input = round(user_Temp_Input + 273.15, 2)
+    print(f'The temperature in Kelvin is: {user_Temp_Input} K')
 else:
     print(f'The {user_Unit_Input} is not valid please try again. ')
