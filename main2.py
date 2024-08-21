f74d3c3f32d12c487640528b9e8841431762215f
Nelson Malbone <nhmalbone@icloud.com>
2024-08-21 17:46:10 -0400

    08/21/2024 added a new file for a newer project, if statements and user input completed

---
+++ main2.py
@@ -0,0 +1,15 @@
+# piggy back to main.py
+# this time we will print out the following
+# using kelvin conversion
+
+user_Unit_Input = input("Is this conversion for ('K') Kelvin, ('C') Celsius, ('F') Fahrenheit: ")
+user_Temp_Input = input("Enter Temperature to Convert: ")
+
+if user_Unit_Input == 'K':
+    pass
+elif user_Unit_Input == 'C':
+    pass
+elif user_Unit_Input == 'F':
+    pass
+else:
+    print(f'The {user_Unit_Input} is not valid please try again. ')
